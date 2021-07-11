40ed4a1c948033c12469334274dc1488287134ef
Prince Kafui <amexoprince@gmail.com>
2021-07-10 17:05:24 -0900

    react-users

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Users from "./Components/Users/Users";
+import Userform from "./Components/Userforms/Userform";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            users: [
+                // {
+                //   name : "Prince",
+                //   email: "amexoprince@gmail",
+                //   gen: 18,
+                // },
+                // {
+                //   name : "Bella",
+                //   email: "bella@gmail",
+                //   gen: 13,
+                // },
+                // {
+                //   name : "Kafui",
+                //   email: "kafui@gmail.com",
+                //   gen: 16,
+                // },
+            ],
+        };
+    }
+
+    handleAddUser = (newUser) => {
+        this.setState({
+            users: [...this.state.users, newUser],
+        });
+    };
+
+    render() {
+        return ( <
+            div >
+            <
+            Userform addUser = { this.handleAddUser }
+            /> {
+                this.state.users.map((item, index) => {
+                    return ( <
+                        div >
+                        <
+                        Users name = { item.name }
+                        email = { item.email }
+                        gen = { item.gen }
+                        /> <
+                        /div>
+                    );
+                })
+            } <
+            /div>
+        );
+    }
 }
 
 export default App;
---
+++ src/Components/Users/Users.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./Users.css";
+
+class Users extends Component {
+  render(props) {
+    return (
+      <div className="users">
+        <h1>{this.props.name}</h1>
+        <h2> {this.props.email}</h2>
+        <h3>{this.props.gen}</h3>
+      </div>
+    );
+  }
+}
+
+export default Users;
